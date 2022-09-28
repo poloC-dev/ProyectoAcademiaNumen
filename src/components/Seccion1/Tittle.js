2780af6a305af77b2198ee37327aa457e94ffee9
poloC-dev <polochariatti@gmail.com>
2022-09-28 15:37:10 -0300

    visual update

--- src/components/Seccion1/Tittle.js
+++ src/components/Seccion1/Tittle.js
@@ -1,12 +1,14 @@
 const Tittle = (props) => {
-
-    let {color,nocolor} = props;
+  let { color, nocolor } = props;
 
   return (
-    <div className="text-3xl px-3">
-        <h2>{nocolor}<span className="text-orange-500 ">{color}</span></h2>
+    <div className="text-2xl px-3">
+      <h2>
+        {nocolor}
+        <span className="text-orange-500">{color}</span>
+      </h2>
     </div>
   );
 };
 
-export default Tittle
+export default Tittle;
