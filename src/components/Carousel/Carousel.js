18092fc139080573478212dbf8b25bf42b35a08b
Carolina Reynoso Tartaro <caro.reytar23@gmail.com>
2022-10-01 14:20:54 -0300

    primer carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,86 @@
 import React from 'react'
 import "./carousel.css"
+import Slider from "react-slick"
+import imagen from "./imagesCarousel/food1-01.jpg";
+import imagen2 from "./imagesCarousel/food2.jpg";
+import imagen3 from "./imagesCarousel/food3-01.jpg";
+import imagen4 from "./imagesCarousel/food4-01.jpg";
+import imagen5 from "./imagesCarousel/food5-01.jpg";
+import imagen6 from "./imagesCarousel/food6-01.jpg";
+import imagen7 from "./imagesCarousel/food7-01.jpg";
+import imagen8 from "./imagesCarousel/food8-01.jpg";
+
+// FLECHAS:
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "orange" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "orange"  }}
+      onClick={onClick}
+    />
+  );
+}
 
 const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
   return (
-    <div>Carousel</div>
+    <>
+    <div style={{backgroundColor: 'yellow' }} className="carouselSeccion">
+      <div>
+        <span className='titlesC'>
+          <p className='titleC'>@PremiumBakeryQuality </p>
+          <p className='titleC2'> on Instagram</p>
+        </span>
+          <Slider {...settings} className="carousel">
+            <div>
+              <div style={{ backgroundImage: `url(${imagen})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen2})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen3})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen4})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen5})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen6})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen7})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+            <div>
+              <div style={{ backgroundImage: `url(${imagen8})`, width: 186, height: 186, backgroundSize: 156, backgroundPosition: "center"}} className="imageCarousel"></div>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,60 +5,61 @@ import imagen from "./images/logo.png";
 const Footer = () => {
   return (
     <>
-    <div className="footer">
+    <footer className="footer">
       <span style={{ backgroundImage: `url(${imagen})`, backgroundSize: 110}} className="logo">
       </span>
 
       <ul>
-        <li className="title">Occasions</li>
-        <li className="list">Occasions</li>
-        <li className="list">Working From Home</li>
-        <li className="list">Snack Time</li>
-        <li className="list">Breakfast</li>
-        <li className="list">Vegan Life</li>
-        <li className="list">Baking</li>
-        <li className="list">Gifts & Bundles</li>
-        <li className="list">Easy Cooking</li>
-        <li className="list">All Products</li>
+        <li className="titleF">Occasions</li>
+
+        <li className="listF">Occasions</li>
+        <li className="listF">Working From Home</li>
+        <li className="listF">Snack Time</li>
+        <li className="listF">Breakfast</li>
+        <li className="listF">Vegan Life</li>
+        <li className="listF">Baking</li>
+        <li className="listF">Gifts & Bundles</li>
+        <li className="listF">Easy Cooking</li>
+        <li className="listF">All Products</li>
       </ul>
 
       <ul>
-        <li className="title">All Categories</li>
+        <li className="titleF">All Categories</li>
 
-        <li className="list">Baking</li>
-        <li className="list">Breakfast</li>
-        <li className="list">Condiments</li>
-        <li className="list">Drinks</li>
-        <li className="list">Heat Things Up</li>
-        <li className="list">Meal Solutions</li>
-        <li className="list">Pantry Staples</li>
-        <li className="list">Simmer Sauces</li>
-        <li className="list">Snacks & Treats</li>
-        <li className="list">Spice Up Your Cooking</li>
-        <li className="list">Sweet Tooth</li>
-        <li className="list">Wine & Cheese</li>
-        <li className="list">All Products</li>
+        <li className="listF">Baking</li>
+        <li className="listF">Breakfast</li>
+        <li className="listF">Condiments</li>
+        <li className="listF">Drinks</li>
+        <li className="listF">Heat Things Up</li>
+        <li className="listF">Meal Solutions</li>
+        <li className="listF">Pantry Staples</li>
+        <li className="listF">Simmer Sauces</li>
+        <li className="listF">Snacks & Treats</li>
+        <li className="listF">Spice Up Your Cooking</li>
+        <li className="listF">Sweet Tooth</li>
+        <li className="listF">Wine & Cheese</li>
+        <li className="listF">All Products</li>
       </ul>
 
       <ul>
-        <li className="title">Gifts & Bundles</li>
+        <li className="titleF">Gifts & Bundles</li>
 
-        <li className="list">Sweet Tooth Special</li>
-        <li className="list">Discover Local</li>
-        <li className="list">Baking Box</li>
-        <li className="list">Spicy Heat Lover's Box</li>
-        <li className="list">Snacking Sensations</li>
+        <li className="listF">Sweet Tooth Special</li>
+        <li className="listF">Discover Local</li>
+        <li className="listF">Baking Box</li>
+        <li className="listF">Spicy Heat Lover's Box</li>
+        <li className="listF">Snacking Sensations</li>
       </ul>
 
       <ul>
-        <li className="title">Our Brands</li>
+        <li className="titleF">Our Brands</li>
       </ul>
 
       <ul>
-        <li className="title">About Us</li>
+        <li className="titleF">About Us</li>
       </ul>
 
-    </div>
+    </footer>
     
     </>
     
