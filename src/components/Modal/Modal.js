a9841a520dc05c7aebbf2996bd82272b31ce05a7
rodonieto <rodonieto@gmail.com>
2022-10-01 00:57:35 -0300

    Modal
    Se actualiza Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,35 @@
-import React from 'react'
+
+import React, { useEffect, useRef } from 'react'
+import { Messages } from 'primereact/messages';
 
 const Modal = () => {
+
+  const message = useRef(null);
+
+  useEffect(() => {
+        
+    message.current.show({
+      severity: 'success', sticky: true, content: (
+        <React.Fragment >
+          <div className="ml-2" style={{ fontFamily: "Poppins", fontWeight: "500", color: "black", textAlign: "center" }} > Free Delivery over $100. Gift with Purchase over $150.  </div>
+        </React.Fragment>
+      )
+    });
+
+}); 
+
+
+
   return (
-    <div>Modal</div>
+    
+      <div className="card" style={{ display: "flex", width: "100%", position: "absolute",  flexDirection: "column" }} >
+                                  
+        <Messages ref={message} />
+               
+                                                   
+      </div>
+      
+    
   )
 }
 
