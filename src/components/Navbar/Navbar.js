295232c12373da810e8a23efd5baf834c3067c08
ALBANO-BONZI <bonzi.albano.andres@gmail.com>
2022-10-23 19:47:59 -0300

    ok

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
+((d)=> { 
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
 
-export default Navbar
+$btnMenu.addEventListener("click", (e)=> { 
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active")
+    })
+
+    d.addEventListener("click", e=> {
+        if (!e.target.matches(".menu a"))return false;
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active")
+        
+
+        
+
+    })
+
+ })(document)
+
+ 
+
+
+
