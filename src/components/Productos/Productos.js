18e8d895a31dad70a73e841d04bc01ddb42d5811
rodonieto <112764145+rodonieto@users.noreply.github.com>
2022-10-29 13:44:10 -0300

    Update Products.js: axios link
    SE modifica link de axios http://localhost:5000/products x https://numen-react-vercel-server.vercel.app/products

--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -11,7 +11,7 @@ const Productos = () => {
 
   const getData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/products")
+      const response = await axios.get("https://numen-react-vercel-server.vercel.app/products")
       
     setData(response.data) 
       
