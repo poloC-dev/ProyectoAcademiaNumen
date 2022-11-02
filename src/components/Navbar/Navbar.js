c138d350d6cae75792b0c13a111fdf9835763039
rodonieto <112764145+rodonieto@users.noreply.github.com>
2022-11-02 20:30:13 -0300

    Update Navbar.js
    Logo update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
 
     return (
         <div className="navcontainer">
-          <img className="logo" alt="Logo" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8687897-418c-40db-b92b-af479b5b1cb6/d317hqy-3ed150a3-4955-420c-8403-287ddd5e5a1e.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E4Njg3ODk3LTQxOGMtNDBkYi1iOTJiLWFmNDc5YjViMWNiNlwvZDMxN2hxeS0zZWQxNTBhMy00OTU1LTQyMGMtODQwMy0yODdkZGQ1ZTVhMWUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ulQpVrSkW5eHe4Qh2yPtw-B_tiHay5Etn2Z7qOXH878"/>
+          <img className="logo" alt="Logo" src={require('../Footer/images/logo.png')}/>
           <nav>
             {(toggleMenu || screenWidth > 500) && (
             <ul className="list">
